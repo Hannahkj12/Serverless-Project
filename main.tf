a5f6fc8d33faa965b2585f9c473ec39981b52024
Hannahkj12 <hannahkj2001@gmail.com>
2026-04-14 12:32:22 -0600

    feat: add cloufront

--- main.tf
+++ main.tf
@@ -155,6 +155,47 @@ resource "aws_api_gateway_stage" "prod" {
   stage_name    = "prod"
 }
 
+# --- 5. CLOUDFRONT DISTRIBUTION ---
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id                = "S3-PDC-Stickers"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-PDC-Stickers"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
 # --- OUTPUTS ---
 
 output "api_url" {
@@ -163,4 +204,8 @@ output "api_url" {
 
 output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.site.website_endpoint
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
